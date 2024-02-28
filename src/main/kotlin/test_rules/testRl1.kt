20e83a24eb9be4d7053654fc23fce4d842c6823b
TonyGnk <iis22067@uom.edu.gr>
2024-02-29 00:13:32 +0200

    Add support for the first rule ApplyOneWorkerInSift

---
+++ src/main/kotlin/test_rules/testRl1.kt
@@ -0,0 +1,28 @@
+package test_rules
+
+import Sift
+
+fun test1ApplyOneWorkerInSift() {
+
+    // This is a test of how the limit RL-1 is applied
+    //? Δε γίνεται ένας εργαζόμενος να καλύπτει δύο θέσεις σε μια βάρδια
+
+    //> Δημιουργούμε μια νέα βάρδια που θα έχει 3 θέσεις (Όπως η πρωινή βάρδια)
+    val sift = Sift(3)
+
+    //> Βλέπουμε ότι και η 3 θέσεις (Ranges) έχουν όλους τους εργαζόμενους
+    sift.print()
+
+    //> Υποθέτουμε ότι με κάποιο τρόπο γίνεται ανάθεση των θέσεων 1 και 2 σε δύο εργαζόμενους
+    sift.removeAllWorkersInAnyNotFixedRangeExcept(2)
+    sift.removeAllWorkersInAnyNotFixedRangeExcept(5)
+
+    //> Βλέπουμε ότι η θέση 3 έχει όλους τους εργαζόμενους ακόμη και αυτούς που έχουν ανατεθεί σε άλλες θέσεις
+    sift.print()
+
+    //> Εδώ εφαρμόζουμε τον περιορισμό του RL-1
+    sift.applyOneWorkerInSift()
+
+    //> Μετά την εφαρμογή του περιορισμού η θέση 3 δεν έχει πλέον τους εργαζόμενους που έχουν ανατεθεί σε άλλες θέσεις
+    sift.print()
+}
---
+++ src/test/java/RangeTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RangeTest {
+
+	@Test
+	void isFixed() {
+	}
+
+	@Test
+	void getFirstWorkerId() {
+	}
+
+	@Test
+	void removeWorker() {
+	}
+
+	@Test
+	void removeAllWorkersExcept() {
+	}
+
+	@Test
+	void workerExists() {
+	}
+
+	@Test
+	void print() {
+	}
+}
