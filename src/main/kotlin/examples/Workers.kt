7d04d172bc068bedb6db6262f6409d279ee98fa1
TonyGnk <61454221+TonyGnk@users.noreply.github.com>
2024-07-25 22:50:12 +0300

    Create the foundation of TableCard of module schedule

--- src/main/kotlin/examples/Workers.kt
+++ src/main/kotlin/examples/Workers.kt
@@ -5,16 +5,88 @@ import com.tonyGnk.core.model.worker.Role
 import com.tonyGnk.core.model.worker.Worker
 
 val workersPreSet: List<Worker> = listOf(
-    Worker(id = "1", name = "Γιαννακοβίτης", gender = Gender.MALE, role = Role.SU),
-    Worker(id = "2", name = "Καρυτόπουλος", gender = Gender.MALE, role = Role.SENIOR),
-    Worker(id = "3", name = "Σιδηρόπουλος", gender = Gender.MALE, role = Role.SENIOR),
-    Worker(id = "4", name = "Στογιάννου", gender = Gender.MALE, role = Role.SENIOR),
-    Worker(id = "5", name = "Μαυροκεφαλίδου", gender = Gender.FEMALE, role = Role.STAFF),
-    Worker(id = "6", name = "Γραμμένας", gender = Gender.MALE, role = Role.STAFF),
-    Worker(id = "7", name = "Παπαστεγίου", gender = Gender.MALE, role = Role.STAFF),
-    Worker(id = "8", name = "Συροπούλου", gender = Gender.FEMALE, role = Role.STAFF),
-    Worker(id = "9", name = "Βασιλειάδη", gender = Gender.MALE, role = Role.STAFF),
-    Worker(id = "10", name = "Λουκίδου", gender = Gender.FEMALE, role = Role.STAFF),
-    Worker(id = "11", name = "Μαλλού", gender = Gender.FEMALE, role = Role.STAFF),
-    Worker(id = "12", name = "Χατζοπούλου", gender = Gender.FEMALE, role = Role.STAFF)
+    Worker(
+        id = "1",
+        name = "John",
+        surname = "Williams",
+        gender = Gender.MALE,
+        role = Role.SU
+    ),
+    Worker(
+        id = "2",
+        name = "Jane",
+        surname = "Martinez",
+        gender = Gender.FEMALE,
+        role = Role.SENIOR
+    ),
+    Worker(
+        id = "3",
+        name = "Michael",
+        surname = "Johnson",
+        gender = Gender.MALE,
+        role = Role.SENIOR
+    ),
+    Worker(
+        id = "4",
+        name = "Emily",
+        surname = "Davis",
+        gender = Gender.FEMALE,
+        role = Role.SENIOR
+    ),
+    Worker(
+        id = "5",
+        name = "Doe",
+        surname = "Williams",
+        gender = Gender.MALE,
+        role = Role.STAFF
+    ),
+    Worker(
+        id = "6",
+        name = "Sophia",
+        surname = "Brown",
+        gender = Gender.FEMALE,
+        role = Role.STAFF
+    ),
+    Worker(
+        id = "7",
+        name = "James",
+        surname = "Jones",
+        gender = Gender.MALE,
+        role = Role.STAFF
+    ),
+    Worker(
+        id = "8",
+        name = "Olivia",
+        surname = "Garcia",
+        gender = Gender.FEMALE,
+        role = Role.STAFF
+    ),
+    Worker(
+        id = "9",
+        name = "Ethan",
+        surname = "Smith",
+        gender = Gender.MALE,
+        role = Role.STAFF
+    ),
+    Worker(
+        id = "10",
+        name = "Mia",
+        surname = "Hernandez",
+        gender = Gender.FEMALE,
+        role = Role.STAFF
+    ),
+    Worker(
+        id = "11",
+        name = "Alexander",
+        surname = "Lopez",
+        gender = Gender.MALE,
+        role = Role.STAFF
+    ),
+    Worker(
+        id = "12",
+        name = "Ava",
+        surname = "Wilson",
+        gender = Gender.FEMALE,
+        role = Role.STAFF
+    )
 )
