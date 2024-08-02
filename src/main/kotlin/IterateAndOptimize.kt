7d1eb4be79c0eca091156fbd89c69fd06302ee9e
TonyGnk <61454221+TonyGnk@users.noreply.github.com>
2024-08-02 23:11:38 +0300

    Complete Schedule Result Part IV

--- src/main/kotlin/IterateAndOptimize.kt
+++ src/main/kotlin/IterateAndOptimize.kt
@@ -1,51 +1,59 @@
-//import com.tonyGnk.core.model.scheduler.Range
-//import com.tonyGnk.core.model.time.next
-//import com.tonyGnk.core.model.time.previous
-//import old.printBlue
-//
-//
-//fun previousFunction(
-//    listToOptimize: MutableList<Range>,
-//    updatedListIndexes: List<Int> = listToOptimize.indices.toList()
-//) {
-//    for (index in updatedListIndexes) {
-//        iterateAndOptimize(listToOptimize, index)
-//    }
-//}
-//
-//
-//fun iterateAndOptimize(
-//    listToOptimize: MutableList<Range>,
-//    updatedIndex: Int,
-//) {
-//    printBlue(updatedIndex.toString())
-//    val newUpdatedListIndexes = mutableListOf<Int>()
-//
-//    if (listToOptimize[updatedIndex].possibleWorkers.size == 1) {
-//        printBlue("F")
-//
-//        val fixedWorkerId = listToOptimize[updatedIndex].possibleWorkers[0]
-//        val momentOfIndex = listToOptimize[updatedIndex].moment
-//        val previousMoment = momentOfIndex.previous()
-//        val nextMoment = momentOfIndex.next()
-//
-//        val listOfTheSameMomentIndexes: List<Int> = listToOptimize.filter {
-//            (it.moment == momentOfIndex || it.moment == previousMoment || it.moment == nextMoment) &&
-//                    it.id != updatedIndex
-//        }.map { it.id }
-//        printBlue("+${listOfTheSameMomentIndexes} ")
-//
-//        for (index in listOfTheSameMomentIndexes) {
-//            if (listToOptimize[index].possibleWorkers.contains(fixedWorkerId)) {
-//                listToOptimize[index].possibleWorkers.remove(fixedWorkerId)
-//
-//                newUpdatedListIndexes.add(index)
-//            }
-//        }
-//
-//    }
-//
-//    for (index in newUpdatedListIndexes) {
-//        iterateAndOptimize(listToOptimize, index)
-//    }
-//}
+import com.tonyGnk.core.model.scheduler.Range
+import com.tonyGnk.core.model.time.next
+import com.tonyGnk.core.model.time.previous
+
+
+fun previousFunction(
+    listToOptimize: MutableList<Range>,
+    updatedListIndexes: List<Int> = listToOptimize.indices.toList()
+) {
+    //println("The updated indexes are $updatedListIndexes")
+    for (index in updatedListIndexes) {
+        iterateAndOptimize(listToOptimize, index)
+    }
+}
+
+
+fun iterateAndOptimize(
+    listToOptimize: MutableList<Range>,
+    updatedIndex: Int,
+) {
+    //println("The updated index is $updatedIndex")
+    val newUpdatedListIndexes = mutableListOf<Int>()
+
+    if (listToOptimize[updatedIndex].possibleWorkers.size == 1) {
+
+        val fixedWorker = listToOptimize[updatedIndex].possibleWorkers[0]
+        val momentOfIndex = listToOptimize[updatedIndex].moment
+        val previousMoment = momentOfIndex.previous()
+        val nextMoment = momentOfIndex.next()
+
+
+        val listOfTheSameMomentIndexes: List<Int> = listToOptimize.filter {
+            (it.moment == momentOfIndex || it.moment == previousMoment || it.moment == nextMoment) &&
+                    it.id != updatedIndex
+        }.map { it.id }
+        //printBlue("listOfTheSameMoment $listOfTheSameMomentIndexes \n")
+
+
+        for (index in listOfTheSameMomentIndexes) {
+            if (listToOptimize[index].possibleWorkers.contains(fixedWorker)) {
+                //listToOptimize.print()
+                // print("\n")
+                //listToOptimize[index].possibleWorkers.filter { it.id != fixedWorker.id }
+                //listToOptimize[index] = listToOptimize[index].removeWorker(fixedWorker)
+                listToOptimize[index].possibleWorkers.removeIf { it.id == fixedWorker.id }
+                //listToOptimize.print()
+
+
+                newUpdatedListIndexes.add(index)
+            }
+        }
+        //printDef("newUpdatedListIndexes $newUpdatedListIndexes \n")
+    }
+
+
+    for (index in newUpdatedListIndexes) {
+        iterateAndOptimize(listToOptimize, index)
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,48 +1,90 @@
-//import com.tonyGnk.core.model.scheduler.ProblemResult
-//import examples.exampleTwoDays
+import com.tonyGnk.core.model.scheduler.ProblemResult
+import com.tonyGnk.core.model.scheduler.Range
+import com.tonyGnk.core.model.scheduler.SearchInput
+import com.tonyGnk.core.model.time.Moment
+import com.tonyGnk.core.model.time.Time
+import com.tonyGnk.core.model.worker.workersPreSet
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.DayOfWeek
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.plus
+
+fun main() {
+
+    val week: List<LocalDate> = getWeek(LocalDate(2024, 7, 15)).take(4)
+
+    val startMoment = Moment(date = week.first(), time = Time.MORNING)
+    val end = Moment(date = week.last(), time = Time.NIGHT)
+    val workers = workersPreSet.take(workersPreSet.size / 2)
+
+    val input = SearchInput(
+        start = startMoment, end = end, workers = workers.toMutableList()
+    )
+    var problemOutput: ProblemResult = createScheduleProblem(input)
+
+    when (val problem = problemOutput) {
+        is ProblemResult.Success -> {
+            //print("Problem created:\n")
+            //problem.problem.print()
+            //print("\n")
+        }
+
+        ProblemResult.NotInitialized -> {}
+    }
+
+    if (problemOutput is ProblemResult.Success) {
+        problemOutput = ProblemResult.Success(setValue(problemOutput))
+        //print("Worker Set:\n")
+        problemOutput.problem.print()
+        print("\n")
+    }
+
+    if (problemOutput is ProblemResult.Success) {
+        //problemOutput = liftSchedulerManualOptimizing(problemOutput.problem)
+//        if (problemOutput is ProblemResult.Success) {
+//            //print("Problem after search:\n")
+//            //problemOutput.problem.print()
+//        }
+    }
+}
+
+fun setValue(problem: ProblemResult.Success): List<Range> {
+    return problem.problem.map {
+        if (it.id == 1) {
+            it.setWorker("1")
+        } else {
+            it
+        }
+    }
+}
+
+fun getWeek(mondayDate: LocalDate): List<LocalDate> {
+    return (0 until DayOfWeek.entries.size).map { dayOfWeek ->
+        mondayDate.plus(dayOfWeek, DateTimeUnit.DAY)
+    }
+}
+//fun applySUAtMorning(
+//    ranges: MutableList<RangeNew>,
+//    workers: MutableList<Worker>,
+//    workerLimits: MutableMap<String, Int>
+//): RuleResult {
 //
-//fun main() {
+//    val suWorkerId = workers.find { it.type == Role.SU }?.id
+//    if (suWorkerId == null) return RuleResult.NoChanges
 //
-//    val selectedExampleInput = exampleTwoDays
-//
-//    when (val problemOutput = createProblem(selectedExampleInput)) {
-//        is ProblemResult.Success -> {
-//            print("Problem created\n")
-//            problemOutput.problem.print()
-//            print("\n")
-//
-//            when (val solution = liftSchedulerSearch(problemOutput.problem)) {
-//                is SearchResult.Success -> solution.ranges.print()
-//                is SearchResult.InitialisationError -> println("\nError! InitialisationError")
-//                is SearchResult.RangesWithNoWorkers -> println("\nError! RangesWithNoWorkers")
-//            }
+//    val updatedIndexes = mutableListOf<Int>()
+//    for (range in ranges) {
+//        if (range.moment.time == Time.MORNING && workerLimits[suWorkerId]!! > 0) {
+//            range.setWorker(suWorkerId)
+//            updatedIndexes.add(range.id)
 //        }
 //    }
+//
+//    ranges.forEach {
+//        print("id:${it.id} possibleWorkers: ")
+//        it.possibleWorkers.forEach { worker -> print("${worker.name}, ") }
+//        println("\n${it.moment}")
+//    }
+//
+//    return RuleResult.Success(updatedIndexes)
 //}
-//
-//
-////fun applySUAtMorning(
-////    ranges: MutableList<RangeNew>,
-////    workers: MutableList<Worker>,
-////    workerLimits: MutableMap<String, Int>
-////): RuleResult {
-////
-////    val suWorkerId = workers.find { it.type == Role.SU }?.id
-////    if (suWorkerId == null) return RuleResult.NoChanges
-////
-////    val updatedIndexes = mutableListOf<Int>()
-////    for (range in ranges) {
-////        if (range.moment.time == Time.MORNING && workerLimits[suWorkerId]!! > 0) {
-////            range.setWorker(suWorkerId)
-////            updatedIndexes.add(range.id)
-////        }
-////    }
-////
-////    ranges.forEach {
-////        print("id:${it.id} possibleWorkers: ")
-////        it.possibleWorkers.forEach { worker -> print("${worker.name}, ") }
-////        println("\n${it.moment}")
-////    }
-////
-////    return RuleResult.Success(updatedIndexes)
-////}
